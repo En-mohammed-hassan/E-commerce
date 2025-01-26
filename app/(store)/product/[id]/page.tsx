7abd9dc930f5690118030bd422086804568131b3
EN-mohammed-hassan <en.mhd.has@gmail.com>
2025-01-26 17:50:25 +0300

    product page params correct

--- app/(store)/product/[id]/page.tsx
+++ app/(store)/product/[id]/page.tsx
@@ -3,18 +3,26 @@ import ProductCard from "@/components/Store/ProductCard";
 import ProductDetails from "@/components/Store/ProductDetails";
 import React from "react";
 
-const page = async function ({ params }: { params: { id: string } }) {
+// Define the type for the page's params
+interface PageProps {
+	params: {
+		id: string;
+	};
+}
+
+const page = async ({ params }: PageProps) => {
 	const products = await getFeaturedProducts();
+
 	return (
 		<div>
-			<ProductDetails id={params.id}></ProductDetails>
+			<ProductDetails id={params.id} />
 			<div>
-				<h1 className="text-4xl font-bold tracking-tighter capitalize my-6  ">
+				<h1 className="text-4xl font-bold tracking-tighter capitalize my-6">
 					Featured Products
 				</h1>
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 					{products?.map((product) => (
-						<ProductCard item={product} key={product.id}></ProductCard>
+						<ProductCard item={product} key={product.id} />
 					))}
 				</div>
 			</div>

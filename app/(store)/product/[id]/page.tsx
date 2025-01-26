3272965e184e34f1d0e449f8623b91464a1a3372
EN-mohammed-hassan <en.mhd.has@gmail.com>
2025-01-26 23:55:36 +0300

    add pormis type to props

--- app/(store)/product/[id]/page.tsx
+++ app/(store)/product/[id]/page.tsx
@@ -2,14 +2,12 @@ import getFeaturedProducts from "@/app/lib/getFeaturedProducts";
 import ProductCard from "@/components/Store/ProductCard";
 import ProductDetails from "@/components/Store/ProductDetails";
 import React from "react";
-interface PageProps {
-	params: {
-		id: string;
-	};
-}
-export default async function Page({ params }: PageProps) {
-	const { id } = params;
+type paramsType = Promise<{ id: string }>;
+
+export default async function Page({ params }: { params: paramsType }) {
+	const { id } = await params;
 	const products = await getFeaturedProducts();
+	
 	return (
 		<div>
 			<ProductDetails id={id}></ProductDetails>
